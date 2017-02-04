7ef5cf3ffb3acfa9ecd68ebd1f3dbdc175df5ccb
Eldar Shamukhamedov <shamu1001@gmail.com>
2017-02-03 20:32:24 -0800

    Add ping pong server and tests

---
+++ test.js
@@ -0,0 +1,25 @@
+/* eslint-env mocha */
+/* eslint-disable max-len */
+const expect = require('chai').expect;
+const fetch = require('node-fetch');
+
+if (process.env.NODE_ENV === 'testing') require('./server');
+
+describe('Ping-pong server', () => {
+  it('should return pong on /ping route', () => {
+    return fetch('http://localhost:12345/ping')
+      .then(response => Promise.all([ response.text(), response.status ]))
+      .then(([ response, status ]) => {
+        expect(response).to.equal('Pong');
+        expect(status).to.equal(200);
+      });
+  });
+  it('should 404 everything else', () => {
+    return fetch('http://localhost:12345/')
+      .then(response => Promise.all([ response.text(), response.status ]))
+      .then(([ response, status ]) => {
+        expect(response).to.equal('Not Found');
+        expect(status).to.equal(404);
+      });
+  });
+});
